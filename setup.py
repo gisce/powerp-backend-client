8b641e2e2d7891b92238fe067ec365091ed9ba93
Xavier Barnada <xbarnada@gisce.net>
2017-08-31 13:48:33 +0200

    Bump to v0.1.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ reqs = data.split()
 
 setup(
     name='powerpbackend-client',
-    version='0.1.1',
+    version='0.1.2',
     packages=find_packages(),
     url='https://gitlab.com/gisce/backend-client',
     license='MIT',
